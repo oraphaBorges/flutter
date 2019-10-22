a44fc1a82993e3d52bc02856ae31a1ae8e6cc5cc
Raphael Borges <rah-borges@hotmail.com>
2019-10-22 12:00:22 -0300

    added o appdrawer na blank page

--- lib/app/pages/blank/blank_view.dart
+++ lib/app/pages/blank/blank_view.dart
@@ -1,16 +1,13 @@
+import 'package:BorgitozApp/components/appdrawer,component.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BlankView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          color: Colors.black38,
-          onPressed: () => Navigator.pop(context,false),
-        ),
+        
       ),
     );
   }
